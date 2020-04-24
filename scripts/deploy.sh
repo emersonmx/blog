3203ef9735b3c3e8e3e5020cfe498d2d0dd9a070
Emerson MX <emersonmx@gmail.com>
2020-04-23 21:42:27 -0300

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,14 +3,23 @@
 set -euo pipefail
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+public_dir="$script_dir/../public"
 
 printf "Deploying updates to GitHub...\n"
+
+# Clean public
+rm -rf $public_dir/categories
+rm -rf $public_dir/css
+rm -rf $public_dir/posts
+rm -rf $public_dir/tags
+rm -f ../public/index.*
+rm -f ../public/sitemap.xml
 
 # Build the project.
 hugo -e production
 
 # Go To Public folder
-cd $script_dir/../public
+cd $public_dir
 
 # Add changes to git.
 git add .
