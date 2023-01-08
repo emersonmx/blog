dce5d303f37d8255d6b2ec52d7347a292f4e31ff
Emerson Max de Medeiros Silva <emersonmx@gmail.com>
2023-01-08 02:36:39 -0300

    Add cssnano

--- themes/simple/postcss.config.js
+++ themes/simple/postcss.config.js
@@ -5,7 +5,18 @@ const tailwindConfig = path.resolve(themeDir, "tailwind.config.js");
 
 module.exports = {
     plugins: [
+        require('postcss-import')({
+            path: [
+                path.resolve(themeDir, 'node_modules'),
+                path.resolve(themeDir, 'assets/css'),
+            ],
+        }),
         require('tailwindcss')(tailwindConfig),
         require('autoprefixer'),
+        ...(
+            process.env.HUGO_ENVIRONMENT === 'production'
+                ? [require('cssnano')]
+                : []
+        )
     ]
 }
